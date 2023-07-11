5a63ecc9152318bbaddfe4bca79a6a79ca9eeb0c
Partho Debnath <parthodebnath28@gmail.com>
2023-07-11 17:20:45 +0600

    add LoginScreen UI

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../styles/style.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String routeName = 'login-screen/';
@@ -11,6 +14,50 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          screenBackground(context),
+          Ink(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Get Started With',
+                  style: head1Text(colorDarkBlue),
+                ),
+                const SizedBox(height: 1),
+                Text(
+                  'Learn with rabbil hasan',
+                  style: head6Text(colorLightGray),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: appInputDecoration('Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: appInputDecoration('Password'),
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.done,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: appButtonStyle(),
+                  onPressed: () {},
+                  child: successButtonChild('Login'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
