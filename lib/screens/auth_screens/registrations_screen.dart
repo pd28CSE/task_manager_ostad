fcb1034fb906e0ab64eaee63a24a9ede469f86d7
Partho Debnath <parthodebnath28@gmail.com>
2023-07-11 17:30:01 +0600

    add RegistrationScreen UI

--- lib/screens/auth_screens/registrations_screen.dart
+++ lib/screens/auth_screens/registrations_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../styles/style.dart';
+
 class RegistrationScreen extends StatefulWidget {
   static const String routeName = 'registration-screen/';
   const RegistrationScreen({super.key});
@@ -11,6 +13,77 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          screenBackground(context),
+          SingleChildScrollView(
+            child: Ink(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Join With Us',
+                    style: head1Text(colorDarkBlue),
+                  ),
+                  const SizedBox(height: 1),
+                  Text(
+                    'Learn with rabbil hasan',
+                    style: head6Text(colorLightGray),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: appInputDecoration('First Name'),
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: appInputDecoration('Last Name'),
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: appInputDecoration('Email'),
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: appInputDecoration('Phone'),
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: appInputDecoration('Password'),
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: appInputDecoration('Confirm Password'),
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.done,
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    style: appButtonStyle(),
+                    onPressed: () {},
+                    child: successButtonChild('Registration'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
