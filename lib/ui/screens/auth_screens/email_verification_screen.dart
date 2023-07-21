510de7b8639cdea7b46f3d996acef5b1c09f227e
Partho Debnath <parthodebnath28@gmail.com>
2023-07-21 14:58:36 +0600

    add Global theme in EmailVerificationScreen

--- lib/ui/screens/auth_screens/email_verification_screen.dart
+++ lib/ui/screens/auth_screens/email_verification_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../../styles/style.dart';
+import '../../widgets/screen_background.dart';
 
 class EmailVerificationScreen extends StatefulWidget {
   static const String routeName = 'email-verification-screen/';
@@ -15,41 +15,36 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          screenBackground(context),
-          Ink(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Your Email Address',
-                  style: head1Text(colorDarkBlue),
-                ),
-                const SizedBox(height: 1),
-                Text(
-                  '6 digit verification pin will send to your email address.',
-                  style: head6Text(colorLightGray),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  decoration: appInputDecoration('Email'),
-                  keyboardType: TextInputType.emailAddress,
-                  textInputAction: TextInputAction.done,
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  style: appButtonStyle(),
-                  onPressed: () {},
-                  child: successButtonChild('Next'),
-                ),
-              ],
-            ),
+      body: ScreenBackground(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Your Email Address',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              const SizedBox(height: 1),
+              Text(
+                '6 digit verification pin will send to your email address.',
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.done,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Next'),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
