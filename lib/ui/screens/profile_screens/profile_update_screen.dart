fa55e7241a65fd1edf6ea72967cd1ac120918f50
Partho Debnath <parthodebnath28@gmail.com>
2023-07-25 02:11:45 +0600

    Update SingleChildScrollView using Expanded in ProfileUpdateScreen

--- lib/ui/screens/profile_screens/profile_update_screen.dart
+++ lib/ui/screens/profile_screens/profile_update_screen.dart
@@ -20,132 +20,134 @@ class _ProfileUpdateScreenState extends State<ProfileUpdateScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              const DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                ),
-                child: UserProfileBanner(
-                  fullName: 'Partho Debnath',
-                  userEmail: 'user@example.com',
-                ),
+        child: Column(
+          children: <Widget>[
+            const DecoratedBox(
+              decoration: BoxDecoration(
+                color: Colors.green,
               ),
-              Padding(
+              child: UserProfileBanner(
+                fullName: 'Partho Debnath',
+                userEmail: 'user@example.com',
+              ),
+            ),
+            Expanded(
+              child: Padding(
                 padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      'Update Profile',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        prefixIcon: TextButton(
-                          style: TextButton.styleFrom(
-                            backgroundColor: Colors.black54,
-                            foregroundColor: Colors.white,
-                            shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(5.0),
-                                bottomLeft: Radius.circular(5.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        'Update Profile',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: TextButton(
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.black54,
+                              foregroundColor: Colors.white,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(5.0),
+                                  bottomLeft: Radius.circular(5.0),
+                                ),
                               ),
                             ),
-                          ),
-                          onPressed: () async {
-                            await pickImageFromGallery();
-                          },
-                          child: const Text('Image'),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      initialValue: 'user@example.com',
-                      // decoration: const InputDecoration(
-                      //   labelText: 'Email',
-                      // ),
-                      readOnly: true,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'First Name',
-                      ),
-                      keyboardType: TextInputType.text,
-                      textInputAction: TextInputAction.next,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Last Name',
-                      ),
-                      keyboardType: TextInputType.text,
-                      textInputAction: TextInputAction.next,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Phone',
-                      ),
-                      keyboardType: TextInputType.phone,
-                      textInputAction: TextInputAction.next,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              isPasswordHidden = !isPasswordHidden;
-                            });
-                          },
-                          icon: Visibility(
-                            visible: isPasswordHidden,
-                            replacement: const Icon(Icons.remove_red_eye),
-                            child: const Icon(Icons.visibility_off),
+                            onPressed: () async {
+                              await pickImageFromGallery();
+                            },
+                            child: const Text('Image'),
                           ),
                         ),
                       ),
-                      keyboardType: TextInputType.text,
-                      textInputAction: TextInputAction.next,
-                      obscureText: isPasswordHidden,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Confirm Password',
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              isConfirmPasswordHidden =
-                                  !isConfirmPasswordHidden;
-                            });
-                          },
-                          icon: Visibility(
-                            visible: isConfirmPasswordHidden,
-                            replacement: const Icon(Icons.remove_red_eye),
-                            child: const Icon(Icons.visibility_off),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        initialValue: 'user@example.com',
+                        // decoration: const InputDecoration(
+                        //   labelText: 'Email',
+                        // ),
+                        readOnly: true,
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'First Name',
+                        ),
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Last Name',
+                        ),
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Phone',
+                        ),
+                        keyboardType: TextInputType.phone,
+                        textInputAction: TextInputAction.next,
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isPasswordHidden = !isPasswordHidden;
+                              });
+                            },
+                            icon: Visibility(
+                              visible: isPasswordHidden,
+                              replacement: const Icon(Icons.remove_red_eye),
+                              child: const Icon(Icons.visibility_off),
+                            ),
                           ),
                         ),
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                        obscureText: isPasswordHidden,
                       ),
-                      keyboardType: TextInputType.text,
-                      textInputAction: TextInputAction.done,
-                      obscureText: isConfirmPasswordHidden,
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Update'),
-                    ),
-                  ],
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Confirm Password',
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isConfirmPasswordHidden =
+                                    !isConfirmPasswordHidden;
+                              });
+                            },
+                            icon: Visibility(
+                              visible: isConfirmPasswordHidden,
+                              replacement: const Icon(Icons.remove_red_eye),
+                              child: const Icon(Icons.visibility_off),
+                            ),
+                          ),
+                        ),
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.done,
+                        obscureText: isConfirmPasswordHidden,
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Update'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
